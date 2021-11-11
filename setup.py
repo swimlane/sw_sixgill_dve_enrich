5d8671d37541a15a3bf49188b0e330a452c25a91
mccluret <tyler.mcclure@swimlane.com>
2021-11-11 21:45:57 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from setuptools import setup
 from json import load
 from six import iteritems
 
-def convert(input):
+
+def convert(_input):
     if isinstance(input, dict):
-        return {convert(k): convert(v) for k, v in iteritems(input)}
+        return {convert(k): convert(v) for k, v in iteritems(_input)}
     elif isinstance(input, list):
-        return [convert(element) for element in input]
+        return [convert(element) for element in _input]
     elif isinstance(input, bytes):
         return input.decode()
     else:
         return input
+
 
 with open("package.json") as config_file:
     config = load(config_file, object_hook=convert)
